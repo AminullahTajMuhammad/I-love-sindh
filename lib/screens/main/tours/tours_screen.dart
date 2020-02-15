269ce192d76487c3903515c2cef7fb37318e895c
amin <aminullahtajmuhammad@gmail.com>
2020-02-16 00:47:02 +0500

    Update some changes like reduce size of images

--- lib/screens/main/tours/tours_screen.dart
+++ lib/screens/main/tours/tours_screen.dart
@@ -1,12 +1,7 @@
 import 'dart:convert';
-import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:i_love_sindh/constants/constants.dart';
-import 'package:i_love_sindh/models/cities_model.dart';
-import 'package:i_love_sindh/models/places_models.dart';
 import 'package:i_love_sindh/screens/tour_places/tour_places_screen.dart';
 
 class ToursScreen extends StatefulWidget {
@@ -47,48 +42,50 @@ class LaunchToursState extends State<ToursScreen> {
                 onTap: () => {},
                 child: Container(
                   padding: EdgeInsets.all(10.0),
-                    child: GestureDetector(
-                      onTap: () => {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              var placesList = citiesList[index]['places'] as List;
-                              return TourPlacesScreen(placesList: placesList, city: citiesList[index]['name']);
-                            },
-                          )
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            var placesList = citiesList[index]['places'] as List;
+                            return TourPlacesScreen(placesList: placesList, city: citiesList[index]['name']);
+                          },
                         )
-                      },
-                      child: Card(
-                        elevation: 5,
-                        child: new  Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                            new Image.network(
-                              citiesList[index]['imageUrl'],
-                              fit: BoxFit.cover,
-                              height: 110,
-                            ),
-                            Container(
-                              padding: EdgeInsets.only(top: 15, left: 10),
-                              child: Text(
-                                citiesList[index]['name'],
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18
-                                ),
+                      )
+                    },
+                    child: Card(
+                      elevation: 5,
+                      child: new  Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          new Image.network(
+                            citiesList[index]['imageUrl'],
+                            fit: BoxFit.cover,
+                            height: 110,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top: 15, left: 10),
+                            child: Text(
+                              citiesList[index]['name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    )
-                  ),
-                );
-              }
+                    ),
+                  )
+                ),
+              );
+            }
           );
         },
       )
     );
   }
+
+
 }
